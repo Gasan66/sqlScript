6a5bf56e7224db4c64f8c1d64c2435cede8996b5
Gasan <abduragimov.ga@gmail.com>
2019-12-06 10:52:36 +0500

    543

--- AddAuditRequest.sql
+++ AddAuditRequest.sql
@@ -1,10 +1,10 @@
 use ASUZD
 begin tran createAuditItem
 
-declare @requestId bigint = 18479
-declare @date nvarchar(50) = '2019-12-04 11:10:14.143'
+declare @requestId bigint = 19439
+declare @date nvarchar(50) = '2019-12-06 09:10:14.143'
 declare @userId nvarchar(50) = '865341a5-ee62-4814-8b29-8ca8d7bc5884'
-declare @message nvarchar(max) = N'Изменил значение PaymentSummWithTax на 478770136.78 для имключения ошибки "Согласование не запущено. Сумма долгосрочных оплат не совпадает с указанной ценой лота по извещению/уведомлению." по заявке Инцидент "(Наряд) IT-219749" ссылка https://10.81.35.30/sd/operator/#uuid:serviceCall$57201090'
+declare @message nvarchar(max) = N'Добавил файл "Соглашение преддоговорных переговоров ЗПэфМСП 12833"'
 
 insert into AuditItems
 values (@date,
