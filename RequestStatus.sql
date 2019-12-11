7ee49f9b8c57d8e00ffbdf169d222333e82a767b
Gasan <abduragimov.ga@gmail.com>
2019-12-11 20:52:03 +0500

    some changes

--- RequestStatus.sql
+++ RequestStatus.sql
@@ -6,11 +6,11 @@ select pr.Id,
 from ASUZD.dbo.PurchaseRequests pr
 left join ASUZD.dbo.Status on ASUZD.dbo.Status.id = pr.Status
 left join ASUZD.dbo.Stages on ASUZD.dbo.Stages.id = pr.Stage
-where pr.id = 18711 or pr.Id = 12456
+where pr.id = 19294 or pr.Id = 12811
 
 update ASUZD.dbo.PurchaseRequests
-set Status = 70
-where ASUZD.dbo.PurchaseRequests.Id = 18711
+set Status = 185, Stage = 5
+where ASUZD.dbo.PurchaseRequests.Id = 19294
 
 select pr.Id,
        Status.Description as 'Status',
@@ -18,6 +18,7 @@ select pr.Id,
 from ASUZD.dbo.PurchaseRequests pr
 left join ASUZD.dbo.Status on ASUZD.dbo.Status.id = pr.Status
 left join ASUZD.dbo.Stages on ASUZD.dbo.Stages.id = pr.Stage
-where pr.id = 18711 or pr.Id = 12456
+where pr.id = 19294 or pr.Id = 12811
 
 rollback tran
+-- commit tran
--- Requests.sql
+++ Requests.sql
@@ -19,6 +19,6 @@ left join AuctionCycles ac on pr.Id = ac.RequestId
 left join TechnicalProjectRequests tpr on tpr.AuctionCycleId = ac.Id
 left join TechnicalProjects tp on tpr.TechnicalProjectId = tp.Id
 left join Status s on s.Id = pr.Status
-where pr.Id = 19114 or pr.Id = 13538
+where pr.Id = 20404 or pr.Id = 13538
 
 rollback tran
