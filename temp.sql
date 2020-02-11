c7beba901af585d9ef6389ad41a394a47077dfed
Gasan <abduragimov.ga@gmail.com>
2020-02-11 09:15:02 +0500

    commit3

--- temp.sql
+++ temp.sql
@@ -1,5 +1,14 @@
-select count(*)
-from ChangeLogs
-where ModifiedBy is not NULL
-and getdate() - DateChanged < 30
+with last_active_usr as (
+    select ordered_ch_logs.num,
+           ordered_ch_logs.ModifiedBy,
+           ordered_ch_logs.DateChanged
+    from
+                (select row_number() over (partition by cl.ModifiedBy order by cl.DateChanged desc ) 'num'
+                      , cl.ModifiedBy
+                      , cl.DateChanged
+                from ChangeLogs cl
+                where cl.ModifiedBy is not null) ordered_ch_logs
+    where ordered_ch_logs.num = 1
+) select *
+from last_active_usr
 
