f904468e93528a4de3ba127b4d62e71a55a073cd
Gasan <abduragimov.ga@gmail.com>
2020-02-13 17:29:11 +0500

    commit3

--- temp.sql
+++ temp.sql
@@ -1,4 +1,23 @@
 use ASUZD
-update AspNetUsers
-set Email = ''
-where Email = ''
+begin tran changeLoin
+    update AspNetUsers
+    set UserName = Email
+    from AspNetUsers usr
+    join _log_change_logins log on usr.Email = log.new_login
+    where usr.Email != 'Borisova-KI@rosseti-ural.ru'
+
+    select email, UserName
+    from AspNetUsers usr
+    join _log_change_logins log on usr.Email = log.new_login
+-- rollback tran
+commit tran
+select AspNetUsers.Id, anr.*
+from AspNetUsers
+join AspNetUserRoles ANUR on AspNetUsers.Id = ANUR.UserId
+join AspNetRoles ANR on ANUR.RoleId = ANR.Id
+where Email = 'Borisova-KI@rosseti-ural.ru'
+
+select log.new_login, count(*)
+from _log_change_logins log
+group by log.new_login
+
