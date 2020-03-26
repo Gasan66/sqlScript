8ba30c8fae0e7ce5c6ee5d68e9b827d8dbee39e8
Gasan <abduragimov.ga@gmail.com>
2020-03-26 20:15:24 +0500

    Another commit

--- ComissionMembers.sql
+++ ComissionMembers.sql
@@ -1,9 +1,9 @@
 use ASUZD
 select usr.LastName, usr.FirstName, usr.MiddleName, oldOrder.Position
 from ComissionMembers mem
-inner join (select * from ComissionMembers where Order_Id = 39) oldOrder on oldOrder.ApplicationUserId = mem.ApplicationUserId
+inner join (select * from ComissionMembers where Order_Id = 47) oldOrder on oldOrder.ApplicationUserId = mem.ApplicationUserId
 left join AspNetUsers usr on mem.ApplicationUserId = usr.Id
-where mem.Order_Id in (49)
+where mem.Order_Id in (53)
 order by usr.LastName
 
 begin tran updatePosition
@@ -11,9 +11,9 @@ begin tran updatePosition
     update ComissionMembers
     set Position = oldOrder.Position, Role = oldOrder.Role
     from ComissionMembers mem
-    inner join (select * from ComissionMembers where Order_Id = 41) oldOrder on oldOrder.ApplicationUserId = mem.ApplicationUserId
+    inner join (select * from ComissionMembers where Order_Id = 47) oldOrder on oldOrder.ApplicationUserId = mem.ApplicationUserId
     left join AspNetUsers usr on mem.ApplicationUserId = usr.Id
-    where mem.Order_Id in (47)
+    where mem.Order_Id in (53)
 
     select usr.LastName, usr.FirstName, usr.MiddleName, mem.Position, mem.Role
     from ComissionMembers mem
@@ -25,13 +25,14 @@ rollback tran
 begin tran insertMembers
 
     insert into ComissionMembers
-        select ApplicationUserId, Position, Role, 49
+        select ApplicationUserId, Position, Role, 53
         from ComissionMembers
-        where Order_id = 39
+        where Order_id = 47
 
     select usr.FirstName + ' ' + usr.MiddleName + ' ' + usr.LastName , mem.*
     from ComissionMembers mem
     left join AspNetUsers usr on usr.Id = mem.ApplicationUserId
-    where Order_Id = 49
+    where Order_Id = 53
 
-rollback tran
+-- rollback tran
+commit tran
